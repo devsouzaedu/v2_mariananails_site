da11f0a4574bc7df19bb3b7cc30cf0605d15faea
José Eduardo <dev.souzaedu@gmail.com>
2025-05-26 10:30:48 -0300

    triste

--- src/app/landingpage/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <div className="w-12 h-12 rounded-full border-4 border-white/20 border-t-white animate-spin"></div>
-    </div>
-  );
-} 
