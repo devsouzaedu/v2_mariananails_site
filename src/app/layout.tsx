c782a30350277ed497c9092c9c3382278b418036
José Eduardo <dev.souzaedu@gmail.com>
2025-12-10 12:39:09 -0300

    feat: adiciona Google Tag Manager (GTM-PMSQ8T3F) no layout principal e rota /fature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,9 +73,30 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className={`${funnelSans.variable} ${ptSans.variable} ${playfair.variable} ${instrumentSerif.variable} ${instrumentSans.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PMSQ8T3F');`
+          }}
+        />
+        {/* End Google Tag Manager */}
         <meta name="facebook-domain-verification" content="9jbaojiqhsgx0fwniftpqth4t23hof" />
       </head>
       <body className="min-h-screen">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PMSQ8T3F"
+            height="0" 
+            width="0" 
+            style={{display:'none',visibility:'hidden'}}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {/* Condição para renderizar Navbar e banner apenas se NÃO for landing, ebook, ebookguiado, curso, fature, quiz ou plataforma */}
         {!isLandingPage && !isEbookPage && !isEbookGuiadoPage && !isCursoPage && !isFaturePage && !isQuizPage && !isPlataformaPage && (
           <>
